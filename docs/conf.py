6519b146a1b5b2c82c7354f57c382502489e3197
Barnaby Shearer <b@zi.is>
2021-11-11 10:11:44 +0000

    Simplifiy docs/conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 """Sphinx config."""
 
-import os
-import sys
-from unittest.mock import Mock
-
-sys.modules["hid"] = Mock()
-sys.modules["PyCRC.CRCCCITT"] = Mock()
-sys.path.insert(0, os.path.abspath(".."))
-
 extensions = ["sphinxcontrib.autoprogram", "sphinx.ext.autodoc"]
 
 project = "EFM8 Bootloader"
 copyright = "2017, Barnaby Shearer"
 author = "Barnaby Shearer"
-
-version = "0.0"
-release = "0.0.2"
 
 master_doc = "index"
